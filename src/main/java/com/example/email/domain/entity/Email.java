426600736ff6e69e334eee1b9ae72624ace43f16
giraffenostrils <dreamthre@gamil.com>
2024-06-17 00:35:23 +0900

    [fix]:이메일엔티티 수정

--- src/main/java/com/example/email/domain/entity/Email.java
+++ src/main/java/com/example/email/domain/entity/Email.java
@@ -2,7 +2,7 @@ package com.example.email.domain.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-
+import java.util.Date;
 import java.util.UUID;
 
 @Entity
@@ -18,8 +18,32 @@ public class Email {
     @Column(name ="EMAIL_ID")
     private UUID id;
 
+    @Column(name = "EMAIL_FROM", nullable = true)
+    private String from;
+
+    @Column(name = "EMAIL_REPLY_TO", nullable = true)
+    private String replyTo;
+
+    @Column(name = "EMAIL_TO", nullable = true)
+    private String[] to;
+
+    @Column(name = "EMAIL_CC", nullable = true)
+    private String[] cc;
+
+    @Column(name = "EMAIL_BCC", nullable = true)
+    private String[] bcc;
+
+    @Column(name = "EMAIL_SENT_DATE", nullable = true)
+    private Date sentDate;
+
+    @Column(name = "EMAIL_SUBJECT", nullable = true)
+    private String subject;
+
+    @Column(name = "EMAIL_TEXT", nullable = true)
+    private String text;
+
     @Setter
-    @Column(name = "EMAIL_STATUS")
+    @Column(name = "EMAIL_STATUS", nullable = true)
     private Boolean status;
 
     @ManyToOne
