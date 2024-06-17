057a597b15b70dfe2bc21c7e9b9bd77ce9d82241
giraffenostrils <dreamthre@gamil.com>
2024-06-17 09:35:47 +0900

    엔티티 주석 설명 추가

--- src/main/java/com/example/email/domain/EmailType.java
+++ src/main/java/com/example/email/domain/EmailType.java
@@ -3,7 +3,7 @@ package com.example.email.domain;
 
 public enum EmailType {
     WELCOME("첫방문", "안녕하세요 {name}님, 노다지사이트를 이용해주셔서 감사합니다.{certification} 인증번호를 입력해주세요"),
-    WINNING("당첨", "안녕하세요 {name}님, {game}에 {rank}되셨습니다.");
+    WINNING("당첨", "안녕하세요 {name}님, {game}에 {rank}등 되셨습니다.");
 
     private final String subject;
     private final String bodyTemplate;
--- src/main/java/com/example/email/domain/entity/Email.java
+++ src/main/java/com/example/email/domain/entity/Email.java
@@ -19,32 +19,32 @@ public class Email {
     private UUID id;
 
     @Column(name = "EMAIL_FROM", nullable = true)
-    private String from;
+    private String from;    //보낸 사람의 이메일 주소
 
     @Column(name = "EMAIL_REPLY_TO", nullable = true)
-    private String replyTo;
+    private String replyTo; //답장을 보낼 이메일 주소
 
     @Column(name = "EMAIL_TO", nullable = true)
-    private String[] to;
+    private String[] to;    //수신자 이메일 주소의 배열
 
     @Column(name = "EMAIL_CC", nullable = true)
-    private String[] cc;
+    private String[] cc;    //참조' 수신자로 포함될 이메일 주소 배열
 
     @Column(name = "EMAIL_BCC", nullable = true)
-    private String[] bcc;
+    private String[] bcc;   //'숨은 참조' 수신자로 포함될 이메일 주소 배열
 
     @Column(name = "EMAIL_SENT_DATE", nullable = true)
-    private Date sentDate;
+    private Date sentDate;  //이메일이 전송된 날짜
 
     @Column(name = "EMAIL_SUBJECT", nullable = true)
-    private String subject;
+    private String subject; //이메일의 제목
 
     @Column(name = "EMAIL_TEXT", nullable = true)
-    private String text;
+    private String text;    //이메일의 본문 텍스트
 
     @Setter
     @Column(name = "EMAIL_STATUS", nullable = true)
-    private Boolean status;
+    private Boolean status; //이메일 발송 여부 확인
 
     @ManyToOne
     @JoinColumn(name = "USER_ID", nullable = false)
